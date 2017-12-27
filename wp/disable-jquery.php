1d465c2b1add931a4ed51c32c66ac9c43af736f9
Darryl Yeo <darryl@darryl-yeo.com>
2017-12-27 08:53:44 -0800

    Spacing

--- wp/disable-jquery.php
+++ wp/disable-jquery.php
@@ -1,6 +1,6 @@
 <?php
-add_action( 'wp_default_scripts', function( $scripts ) {
-	if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
+add_action( 'wp_default_scripts', function($scripts) {
+	if ( !is_admin() && !empty( $scripts->registered['jquery'] ) ) {
 		$jquery_dependencies = $scripts->registered['jquery']->deps;
 		$scripts->registered['jquery']->deps = array_diff( $jquery_dependencies, ['jquery-migrate'] );
 	}
