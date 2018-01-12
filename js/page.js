3e76a0ac55fdf414291a74f7f58172d9695ff78b
Darryl Yeo <darryl@darryl-yeo.com>
2018-01-12 14:51:19 -0800

    Check if "opengraph" meta exists

--- js/page.js
+++ js/page.js
@@ -36,12 +36,16 @@ const DYPage = {
 			'title': title,
 			'meta[name=description][content]': metadesc,
 			'link[rel=canonical][href]': canonical,
-			'meta[property="og:title"][content]': opengraph.title,
-			'meta[property="og:description"][content]': opengraph.description,
-			'meta[property="og:image"][content]': opengraph.image.urls[0],
-			'meta[property="og:image:width"][content]': opengraph.image.dimensions.width,
-			'meta[property="og:image:height"][content]': opengraph.image.dimensions.height
 		})
+		if(opengraph){
+			document.head.updateWithModel({
+				'meta[property="og:title"][content]': opengraph.title,
+				'meta[property="og:description"][content]': opengraph.description,
+				'meta[property="og:image"][content]': opengraph.image.urls[0],
+				'meta[property="og:image:width"][content]': opengraph.image.dimensions.width,
+				'meta[property="og:image:height"][content]': opengraph.image.dimensions.height
+			})
+		}
 
 		//if(!this.$yoastSEO) this.$yoastSEO = $$$('span').appendTo(document.head)
 		//this.$yoastSEO.html(head)
