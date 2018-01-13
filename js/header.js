4dbd1bddde6b62c1b01683aa79427d6eb1b99999
Darryl Yeo <darryl@darryl-yeo.com>
2018-01-12 16:29:54 -0800

    Smart sticky <header> and <dy-sidebar> positioning

--- js/header.js
+++ js/header.js
@@ -1,10 +1,23 @@
 const $header = $('header')
 
 {
-	const observer = new IntersectionObserver(([entry]) => {
-		$header.toggleClass(!entry.isIntersecting, 'sticky')
+	let isSticky = false
+	function updateHeader(){
+		$header
+			.toggleClass(isSticky, 'sticky')
+			.toggleClass(isSticky && WP.queryType === 'single', 'show-post-nav')
+	}
+
+	const observer = new IntersectionObserver(entries => {
+		for(const entry of entries){
+			isSticky = entry.isIntersecting
+			updateHeader()
+		}
 	}, {
-		threshold: 0
+		threshold: 0,
+		rootMargin: '75% 0% -175% 0%' // <body> top reaches 75vh above viewport
 	})
-	observer.observe($('#secondary-nav'))
+	observer.observe($('body'))
+	
+	$header.addClass('sticky-open')
 }
