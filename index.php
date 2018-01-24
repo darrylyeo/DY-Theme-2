19023440d433ee65a28729cd2ceaa3c91e4f22f4
Darryl Yeo <darryl@darryl-yeo.com>
2018-01-24 10:05:38 -0800

    Add "resize-everything" experiment

--- index.php
+++ index.php
@@ -47,6 +47,7 @@
 					<div><a data-experiment="ajax-navigation"><i icon="compass"></i>AJAX Navigation</a></div>
 					<div><a data-experiment="pin-window"><i icon="pin"></i>Pin Window</a></div>
 					<div><a data-experiment="edit-mode"><i icon="pencil"></i>Edit Mode</a></div>
+					<div><a data-experiment="resize-everything"><i icon="resize"></i>Resize Everything</a></div>
 				</div>
 			</div>
 		</nav>
