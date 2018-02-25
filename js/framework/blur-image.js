728114f958c30a877a75762e9eb8226eb87a48ec
Darryl Yeo <darryl@darryl-yeo.com>
2018-02-25 15:49:06 -0800

    Rename variables; reuse canvas element

--- js/framework/blur-image.js
+++ js/framework/blur-image.js
@@ -227,30 +227,32 @@ const stackBlur = (imageData, radius) => {
 stackBlur.mulTable = [512,512,456,512,328,456,335,512,405,328,271,456,388,335,292,512,454,405,364,328,298,271,496,456,420,388,360,335,312,292,273,512,482,454,428,405,383,364,345,328,312,298,284,271,259,496,475,456,437,420,404,388,374,360,347,335,323,312,302,292,282,273,265,512,497,482,468,454,441,428,417,405,394,383,373,364,354,345,337,328,320,312,305,298,291,284,278,271,265,259,507,496,485,475,465,456,446,437,428,420,412,404,396,388,381,374,367,360,354,347,341,335,329,323,318,312,307,302,297,292,287,282,278,273,269,265,261,512,505,497,489,482,475,468,461,454,447,441,435,428,422,417,411,405,399,394,389,383,378,373,368,364,359,354,350,345,341,337,332,328,324,320,316,312,309,305,301,298,294,291,287,284,281,278,274,271,268,265,262,259,257,507,501,496,491,485,480,475,470,465,460,456,451,446,442,437,433,428,424,420,416,412,408,404,400,396,392,388,385,381,377,374,370,367,363,360,357,354,350,347,344,341,338,335,332,329,326,323,320,318,315,312,310,307,304,302,299,297,294,292,289,287,285,282,280,278,275,273,271,269,267,265,263,261,259]
 stackBlur.shgTable = [9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16, 17,17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19,19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20,20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21,21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21,21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22,22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22,22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23,23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24];
 
+{
+const $canvas = document.createElement('canvas')
+const ctx = $canvas.getContext('2d')
 
-const blurImage = (src, blur = 17) => new Promise(resolve => {
-	const img = document.createElement('img')
-	img.src = src
-	const c = document.createElement('canvas')
-	const ctx = c.getContext('2d')
-
-	img.onload = () => requestAnimationFrame(() => {
+var blurImage = (src, blur = 17) => new Promise(resolve => {
+	const $img = document.createElement('img')
+	$img.src = src
+	$img.onload = () => requestAnimationFrame(() => {
 		/*const offset = blur
 		const width = c.width = img.naturalWidth - offset * 2
 		const height = c.height = img.naturalHeight - offset * 2
 		ctx.drawImage(img, -offset, -offset, width, height)*/
 		
-		const width = c.width = Math.min(img.naturalWidth, 450)
-		const height = c.height = Math.floor(img.naturalHeight / img.naturalWidth * width)
+		const width = $canvas.width = Math.min($img.naturalWidth, 450)
+		const height = $canvas.height = Math.floor($img.naturalHeight / $img.naturalWidth * width)
 		if(ctx.filter){
 			ctx.filter = `blur(${blur}px)`
 		}
-		ctx.drawImage(img, 0, 0, width, height)
+		ctx.drawImage($img, 0, 0, width, height)
 		if(!ctx.filter){
 			const imageData = ctx.getImageData(0, 0, width, height)
 			ctx.putImageData(stackBlur(imageData, blur), 0, 0)
 		}
 
-		resolve(`url(${c.toDataURL()})`)
+		resolve(`url(${$canvas.toDataURL()})`)
 	})
 })
+
+}
