19a3a6280f3a233d1da7781f3166c6e31e54df33
Darryl Yeo <darryl@darryl-yeo.com>
2018-02-28 15:09:44 -0800

    Remove deprecated code

--- functions.php
+++ functions.php
@@ -52,21 +52,3 @@ add_action('wp_enqueue_scripts', function(){
 		'restNonce' => wp_create_nonce( 'wp_rest' )
 	]);
 });
-
-
-/*
-<script src="<?= get_theme_file_uri('js/libraries/custom-elements.min.js') ?>"></script>
-<script src="<?= get_theme_file_uri('js/libraries/web-animations.min.js') ?>"></script>
-<script src="<?= get_theme_file_uri('js/framework/framework.js') ?>"></script>
-<script src="<?= get_theme_file_uri('assets.js') ?>"></script>
-<script>
-const WP = <?= json_encode([
-	'siteURL' => WP_SITEURL,
-	'themes' => get_theme_root_uri(),
-	'parentTheme' => get_template_directory_uri(),
-	'childTheme' => get_stylesheet_directory_uri(),
-	'rest' => untrailingslashit(get_rest_url()),
-	'restNonce' => wp_create_nonce( 'wp_rest' )
-]) ?>
-</script>
-*/
