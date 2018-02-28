32c61ab218cb94b002ebbcefccfa179de9292280
Darryl Yeo <darryl@darryl-yeo.com>
2018-02-27 21:06:15 -0800

    Move DY data methods inside object literal

--- js/data.js
+++ js/data.js
@@ -5,164 +5,164 @@ URL.pathName = function(url){
 	return (new URL(url)).pathname
 }
 
-const DY = {}
-
-DY.getDatabase = (async () => {
-	const DB_VERSION = 1
-
-	let updateNeeded = false
-	
-	const db = await indexedDB.openDatabase('Darryl-Yeo', DB_VERSION, {
-		'data': {
-			keyPath: 'key'
-		},
-
-		'wp-objects': {
-			keyPath: 'link',
-			//autoIncrement: true,
-			indices: {
-				'link': {
-					unique: true
-				},
-				'id': {
-					unique: true
-				},
-				'term_id': {
-					unique: true
-				},
-				'objectType': {
-					unique: false
-				},
-				'date_gmt': {
-					unique: false
-				},
-				'modified_gmt': {
-					unique: false
-				}
-			}
-		},
-
-		'wp-taxonomies': {
-			keyPath: 'order',
-			indices: {
-				'slug': {
-					unique: true
-				}
-			}
-		}
-	}, {
-		upgrade(){
-			updateNeeded = true
-		}
-	})
-
-	X('Database accessed.')
-
-	if(updateNeeded){
-		X('Database has been upgraded. Waiting to update data...')
-		await DY.updateDatabase(db)
-		X('Data updated.')
-	}else{
-		X('Updating data in background...')
-		DY.updateDatabase(db).then(() => X('Data updated.'))
-	}
-
-	return db
-})()
-
-
-DY.updateDatabase = async db => {
-	const [
-		pages,
-		posts,
-		taxonomies,
-		termsByTaxonomy
-	] = await Promise.all([
-		getJSON(`${WP.rest}/pages?per_page=100`),
-		getJSON(`${WP.rest}/posts?per_page=100&post_status=published`),
-		getJSON(`${WP.rest}/taxonomies`).then(Object.values),
-		getJSON(`${WP.rest}/terms`)
-	])
-
-	db = db || await DY.getDatabase
-
-	const terms = Object.values(termsByTaxonomy).flatten()
-	const projectsCategory = termsByTaxonomy['page-category'].find(term => term.slug === 'project')
-	const PROJECT_CATEGORY_ID = projectsCategory.term_id
-
-	const objectsStore = db.getObjectStore('wp-objects', true)
-	function putObject(object, objectType){
-		object.objectType = objectType
-		objectsStore.put(object)
-	}
-
-	for(const page of pages){
-		putObject(page, page.terms.includes(PROJECT_CATEGORY_ID) ? 'project' : 'page')
-	}
-	for(const post of posts) putObject(post, 'post')
-	for(const term of terms) putObject(term, 'term')
-
-
-	const taxonomiesStore = db.getObjectStore('wp-taxonomies', true)
-	for(const [i, taxonomy] of taxonomies.entries()){
-		taxonomy.order = i
-		taxonomiesStore.put(taxonomy)
-	}
-
-	return db
-}
-
-DY.getObjectForURL = async url => (await DY.getDatabase)
-	.getObjectStore('wp-objects')
-	.index('link')
-	.get(url)
-
-DY.getObjectsByType = async type => (await DY.getDatabase)
-	.getObjectStore('wp-objects')
-	.index('objectType')
-	.getAll(type)
-
-DY.getTermById = async id => (await DY.getDatabase)
-	.getObjectStore('wp-objects')
-	.index('term_id')
-	.get(id)
-
-DY.getTaxonomies = async () => (await DY.getDatabase)
-	.getObjectStore('wp-taxonomies')
-	.getAll()
-
-
-DY.getUser = fetch(`${WP.rest}/users/me?context=edit`, {
-	credentials: 'same-origin',
-	headers: {
-		'X-WP-Nonce': WP.restNonce
-	}
-})
-	.then(async response => {
-		const data = await response.json()
-		X(response)
-		if(response.status === 200){
-			return data
-		}else if(response.status === 401){
-			throw data
-		}
-	})
-	.catch(e => {
-		X(e.message, e)
-		return
-	})
-
 {
-const data = {}
+let data = {}
 
-DY.data = async key => {
-	if(key in data) return data[key]
+var DY = {
+	getDatabase: (async () => {
+		const DB_VERSION = 1
 
-	const dataObj = await (await DY.getDatabase).getObjectStore('data').get(key)
-	const value = dataObj && dataObj.value || {}
-	data[key] = value
-	return value
+		let updateNeeded = false
+		
+		const db = await indexedDB.openDatabase('Darryl-Yeo', DB_VERSION, {
+			'data': {
+				keyPath: 'key'
+			},
+
+			'wp-objects': {
+				keyPath: 'link',
+				//autoIncrement: true,
+				indices: {
+					'link': {
+						unique: true
+					},
+					'id': {
+						unique: true
+					},
+					'term_id': {
+						unique: true
+					},
+					'objectType': {
+						unique: false
+					},
+					'date_gmt': {
+						unique: false
+					},
+					'modified_gmt': {
+						unique: false
+					}
+				}
+			},
+
+			'wp-taxonomies': {
+				keyPath: 'order',
+				indices: {
+					'slug': {
+						unique: true
+					}
+				}
+			}
+		}, {
+			upgrade(){
+				updateNeeded = true
+			}
+		})
+
+		X('Database accessed.')
+
+		if(updateNeeded){
+			X('Database has been upgraded. Waiting to update data...')
+			await DY.updateDatabase(db)
+			X('Data updated.')
+		}else{
+			X('Updating data in background...')
+			DY.updateDatabase(db).then(() => X('Data updated.'))
+		}
+
+		return db
+	})(),
+
+	updateDatabase: async db => {
+		const [
+			pages,
+			posts,
+			taxonomies,
+			termsByTaxonomy
+		] = await Promise.all([
+			getJSON(`${WP.rest}/pages?per_page=100`),
+			getJSON(`${WP.rest}/posts?per_page=100&post_status=published`),
+			getJSON(`${WP.rest}/taxonomies`).then(Object.values),
+			getJSON(`${WP.rest}/terms`)
+		])
+
+		db = db || await DY.getDatabase
+
+		const terms = Object.values(termsByTaxonomy).flatten()
+		const projectsCategory = termsByTaxonomy['page-category'].find(term => term.slug === 'project')
+		const PROJECT_CATEGORY_ID = projectsCategory.term_id
+
+		const objectsStore = db.getObjectStore('wp-objects', true)
+		function putObject(object, objectType){
+			object.objectType = objectType
+			objectsStore.put(object)
+		}
+
+		for(const page of pages){
+			putObject(page, page.terms.includes(PROJECT_CATEGORY_ID) ? 'project' : 'page')
+		}
+		for(const post of posts) putObject(post, 'post')
+		for(const term of terms) putObject(term, 'term')
+
+
+		const taxonomiesStore = db.getObjectStore('wp-taxonomies', true)
+		for(const [i, taxonomy] of taxonomies.entries()){
+			taxonomy.order = i
+			taxonomiesStore.put(taxonomy)
+		}
+
+		return db
+	},
+	
+	getObjectForURL: async url => (await DY.getDatabase)
+		.getObjectStore('wp-objects')
+		.index('link')
+		.get(url),
+
+	getObjectsByType: async type => (await DY.getDatabase)
+		.getObjectStore('wp-objects')
+		.index('objectType')
+		.getAll(type),
+
+	getTermById: async id => (await DY.getDatabase)
+		.getObjectStore('wp-objects')
+		.index('term_id')
+		.get(id),
+
+	getTaxonomies: async () => (await DY.getDatabase)
+		.getObjectStore('wp-taxonomies')
+		.getAll(),
+
+
+	getUser: fetch(`${WP.rest}/users/me?context=edit`, {
+		credentials: 'same-origin',
+		headers: {
+			'X-WP-Nonce': WP.restNonce
+		}
+	})
+		.then(async response => {
+			const data = await response.json()
+			// X(response)
+			if(response.status === 200){
+				return data
+			}else if(response.status === 401){
+				throw data
+			}
+		})
+		.catch(e => {
+			X(e.message, e)
+			return
+		}),
+
+	data: async key => {
+		if(key in DY.data) return data[key]
+
+		const dataObj = await (await DY.getDatabase).getObjectStore('data').get(key)
+		const value = dataObj && dataObj.value || {}
+		DY.data[key] = value
+		return value
+	}
 }
+
 window.on({
 	beforeunload: () => {
 		(async () => {
@@ -179,4 +179,5 @@ window.on({
 		})()
 	}
 })
+
 }
