58ec57e0f2c157993b57114ed6a13b5234f8b997
Darryl Yeo <darryl@darryl-yeo.com>
2018-04-17 07:43:12 -0700

    Message for browsers that don't support ES6

--- assets-load.js
+++ assets-load.js
@@ -1,3 +1,20 @@
+var DETECT = {
+	es2015: true
+}
+try { eval(`async () => {}`) } catch(e) { DETECT.es2015 = false }
+
+
+if(DETECT.es2015){
+	loadAssets(ASSETS)
+	// loadAssets(ASSETS_COMPILED)
+	// loadAssets(ASSETS_COMPILED_MINIFIED)
+}else{
+	// Message for unsupported browsers
+	document.body.innerText = `Welcome to my site! Unfortunately, you'll need to use a newer browser.`
+	document.documentElement.addClass('load')
+}
+
+
 function loadAssets(ASSETS){
 	for(const path of ASSETS.CSS) document.head.appendChild(Object.assign(document.createElement('link'), {
 		rel: 'stylesheet',
@@ -9,5 +26,3 @@ function loadAssets(ASSETS){
 		async: false
 	}))
 }
-loadAssets(ASSETS)
-//loadAssets(ASSETS_COMPILED)
