86fd3b3cecb75a6e3c8bcff4014370029b22c577
Darryl Yeo <darryl@darryl-yeo.com>
2018-04-29 16:18:06 -0700

    Don't strip attributes; code styling

--- wp/post-content-sections.php
+++ wp/post-content-sections.php
@@ -1,23 +1,18 @@
 <?php
 add_filter('the_content', function($content){
-	$content = '<section id="top">'.preg_replace_callback(
-		'/<h2(?:.*?)>(.*?)<\/h2>/',
-		function($matches){
-			$title = $matches[1];
-			$id = sanitize_title($title);
-			return "</section><section id={$id}><h2><a href=#{$id} class=anchor>{$title}</a></h2>";
-		},
-		$content
-	).'</section>';
-	$content = preg_replace_callback(
-		'/<h3(?:.*?)>(.*?)<\/h3>/',
-		function($matches){
-			$title = $matches[1];
-			$id = sanitize_title($title);
-			return "<h3 id={$id}><a href=#{$id} class=anchor>{$title}</a></h3>";
-		},
-		$content
-	);	
+	$content = '<section id="top">'.preg_replace_callback('/<h2(.*?)>(.*?)<\/h2>/', function($matches){
+		list(, $attrs, $title) = $matches;
+		$id = sanitize_title($title);
+		return "</section><section id={$id}><h2{$attrs}><a href=#{$id} class=anchor>{$title}</a></h2>";
+	}, $content).'</section>';
+
+	$content = preg_replace_callback('/<h3(.*?)>(.*?)<\/h3>/', function($matches){
+		list(, $attrs, $title) = $matches;
+		$id = sanitize_title($title);
+		return "<h3 id={$id}{$attrs}><a href=#{$id} class=anchor>{$title}</a></h3>";
+	}, $content);
+
 	$content = str_replace('<section></section>', '', $content);
+	
 	return $content;
 });
